5325f5ca293f745bfbc1ee11fe93dae7826015f1
LEE SangHoon <havel15x@naver.com>
2025-09-01 16:53:52 +0900

    Hex Map Part 12
    +Saving and Loading
    Keep track of terrain type, instead of color.
    Create a file.
    Write data to a file, then read it.
    Serialize cell data.
    Reduce file size.

--- Assets/Scripts/HexGrid.cs
+++ Assets/Scripts/HexGrid.cs
@@ -1,11 +1,10 @@
-﻿using UnityEngine;
+﻿using System.IO;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class HexGrid : MonoBehaviour {
 
 	public int chunkCountX = 4, chunkCountZ = 3;
-
-	public Color defaultColor = Color.white;
 
 	public HexCell cellPrefab;
 	public Text cellLabelPrefab;
@@ -20,9 +19,12 @@ public class HexGrid : MonoBehaviour {
 
     public int seed;
 
+    public Color[] colors;
+
     void Awake () {
 		HexMetrics.noiseSource = noiseSource;
         HexMetrics.InitializeHashGrid(seed);
+        HexMetrics.colors = colors;
 
         cellCountX = chunkCountX * HexMetrics.chunkSizeX;
 		cellCountZ = chunkCountZ * HexMetrics.chunkSizeZ;
@@ -56,6 +58,7 @@ public class HexGrid : MonoBehaviour {
         if (!HexMetrics.noiseSource) {
             HexMetrics.noiseSource = noiseSource;
             HexMetrics.InitializeHashGrid(seed);
+            HexMetrics.colors = colors;
         }
     }
 
@@ -94,7 +97,6 @@ public class HexGrid : MonoBehaviour {
 		HexCell cell = cells[i] = Instantiate<HexCell>(cellPrefab);
 		cell.transform.localPosition = position;
 		cell.coordinates = HexCoordinates.FromOffsetCoordinates(x, z);
-		cell.Color = defaultColor;
 
 		if (x > 0) {
 			cell.SetNeighbor(HexDirection.W, cells[i - 1]);
@@ -134,4 +136,19 @@ public class HexGrid : MonoBehaviour {
 		int localZ = z - chunkZ * HexMetrics.chunkSizeZ;
 		chunk.AddCell(localX + localZ * HexMetrics.chunkSizeX, cell);
 	}
+
+    public void Save(BinaryWriter writer) {
+        for (int i = 0; i < cells.Length; i++) {
+            cells[i].Save(writer);
+        }
+    }
+
+    public void Load(BinaryReader reader) {
+        for (int i = 0; i < cells.Length; i++) {
+            cells[i].Load(reader);
+        }
+        for (int i = 0; i < chunks.Length; i++) {
+            chunks[i].Refresh();
+        }
+    }
 }
