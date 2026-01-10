313e9df18fd22a06dbf71c2036546d6fe5efe836
LEE SangHoon <havel15x@naver.com>
2026-01-10 22:32:28 +0900

    Hex Map 3.0.0
    Only store cells in a single place.
    Eliminate cell game objects.

--- Assets/Scripts/UI/HexGameUI.cs
+++ Assets/Scripts/UI/HexGameUI.cs
@@ -4,80 +4,82 @@ using UnityEngine.EventSystems;
 /// <summary>
 /// Component that manages the game UI.
 /// </summary>
-public class HexGameUI : MonoBehaviour {
+public class HexGameUI : MonoBehaviour
+{
+    [SerializeField]
+    HexGrid grid;
 
-	[SerializeField]
-	HexGrid grid;
+    int currentCellIndex = -1;
 
-	HexCell currentCell;
+    HexUnit selectedUnit;
 
-	HexUnit selectedUnit;
+    /// <summary>
+    /// Set whether map edit mode is active.
+    /// </summary>
+    /// <param name="toggle">Whether edit mode is enabled.</param>
+    public void SetEditMode(bool toggle) {
+        enabled = !toggle;
+        grid.ShowUI(!toggle);
+        grid.ClearPath();
+        if (toggle) {
+            Shader.EnableKeyword("_HEX_MAP_EDIT_MODE");
+        }
+        else {
+            Shader.DisableKeyword("_HEX_MAP_EDIT_MODE");
+        }
+    }
 
-	/// <summary>
-	/// Set whether map edit mode is active.
-	/// </summary>
-	/// <param name="toggle">Whether edit mode is enabled.</param>
-	public void SetEditMode (bool toggle) {
-		enabled = !toggle;
-		grid.ShowUI(!toggle);
-		grid.ClearPath();
-		if (toggle) {
-			Shader.EnableKeyword("_HEX_MAP_EDIT_MODE");
-		}
-		else {
-			Shader.DisableKeyword("_HEX_MAP_EDIT_MODE");
-		}
-	}
+    void Update() {
+        if (!EventSystem.current.IsPointerOverGameObject()) {
+            if (Input.GetMouseButtonDown(0)) {
+                DoSelection();
+            }
+            else if (selectedUnit) {
+                if (Input.GetMouseButtonDown(1)) {
+                    DoMove();
+                }
+                else {
+                    DoPathfinding();
+                }
+            }
+        }
+    }
 
-	void Update () {
-		if (!EventSystem.current.IsPointerOverGameObject()) {
-			if (Input.GetMouseButtonDown(0)) {
-				DoSelection();
-			}
-			else if (selectedUnit) {
-				if (Input.GetMouseButtonDown(1)) {
-					DoMove();
-				}
-				else {
-					DoPathfinding();
-				}
-			}
-		}
-	}
+    void DoSelection() {
+        grid.ClearPath();
+        UpdateCurrentCell();
+        if (currentCellIndex >= 0) {
+            selectedUnit = grid.GetCell(currentCellIndex).Unit;
+        }
+    }
 
-	void DoSelection () {
-		grid.ClearPath();
-		UpdateCurrentCell();
-		if (currentCell) {
-			selectedUnit = currentCell.Unit;
-		}
-	}
+    void DoPathfinding() {
+        if (UpdateCurrentCell()) {
+            if (currentCellIndex >= 0 &&
+                selectedUnit.IsValidDestination(grid.GetCell(currentCellIndex))) {
+                grid.FindPath(
+                    selectedUnit.Location, grid.GetCell(currentCellIndex), selectedUnit);
+            }
+            else {
+                grid.ClearPath();
+            }
+        }
+    }
 
-	void DoPathfinding () {
-		if (UpdateCurrentCell()) {
-			if (currentCell && selectedUnit.IsValidDestination(currentCell)) {
-				grid.FindPath(selectedUnit.Location, currentCell, selectedUnit);
-			}
-			else {
-				grid.ClearPath();
-			}
-		}
-	}
+    void DoMove() {
+        if (grid.HasPath) {
+            selectedUnit.Travel(grid.GetPath());
+            grid.ClearPath();
+        }
+    }
 
-	void DoMove () {
-		if (grid.HasPath) {
-			selectedUnit.Travel(grid.GetPath());
-			grid.ClearPath();
-		}
-	}
-
-	bool UpdateCurrentCell () {
-		HexCell cell =
-			grid.GetCell(Camera.main.ScreenPointToRay(Input.mousePosition));
-		if (cell != currentCell) {
-			currentCell = cell;
-			return true;
-		}
-		return false;
-	}
+    bool UpdateCurrentCell() {
+        HexCell cell = grid.GetCell(Camera.main.ScreenPointToRay(Input.mousePosition));
+        int index = cell ? cell.Index : -1;
+        if (index != currentCellIndex) {
+            currentCellIndex = index;
+            return true;
+        }
+        return false;
+    }
 }
